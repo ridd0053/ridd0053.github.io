0963da4d148fb94aba03ee8c9d5156fa086b79e7
ridd0053 <ridd0053@hz.nl>
2019-09-05 13:31:41 +0200

    edit

---
+++ punten.js
@@ -0,0 +1,7 @@
+let ec = document.querySelector('#EC');
+let punten = document.querySelector('#punten');
+const btn = document.querySelector('.btn');
+
+function mijnfunctie(){
+ punten.value = ec.value
+}
